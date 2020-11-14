665459bb9bd434f7047e5ce7c69ecd61c9c62f65
Paulo César <paulo_cesarcanal@hotmail.com>
2020-11-14 18:35:55 -0300

    COD JS COM DEFEITO

---
+++ html/javascript/script.js
@@ -0,0 +1,8 @@
+const bolas = document.getElementsByClassName('ball');
+
+function numeroAleatorio(inicio, fim) {
+    cont numero = Math.floor(Math.random());
+    return numero;
+};
+
+console.log(numeroAleatorio());
